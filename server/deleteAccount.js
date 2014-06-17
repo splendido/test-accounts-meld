4654122efd685e03ff7896dd6fb2c9a65bfb4071
Luca Mussi <ing.luca.mussi@gmail.com>
2014-06-17 09:17:15 +0200

    fixed deleteAccount

--- server/deleteAccount.js
+++ server/deleteAccount.js
@@ -4,6 +4,12 @@ Meteor.methods({
 		if (userId){
 			Notes.remove({user_id: userId});
 			Meteor.users.remove({_id: userId});
+			MeldActions.remove({
+				$or: [
+					{dst_user_id: userId},
+					{src_user_id: userId}
+				]
+			});
 		}
 	}
 });
