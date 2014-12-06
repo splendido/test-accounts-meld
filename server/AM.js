d73947975bae4805f23612f7d69d0608fd61edc0
Luca Mussi <ing.luca.mussi@gmail.com>
2014-12-06 16:39:09 +0100

    updates

--- server/AM.js
+++ server/AM.js
@@ -1,8 +1,19 @@
 var meldDBCallback = function(src_user_id, dst_user_id){
-	Notes.update({user_id: src_user_id}, {$set: {user_id: dst_user_id}}, {multi: true});
+    Notes.update({user_id: src_user_id}, {$set: {user_id: dst_user_id}}, {multi: true});
+};
+
+var serviceAddedCallback = function(user_id, service_name){
+    if (service_name === 'facebook'){
+        console.log('Facebook just added for user ' + user_id);
+        var user = Meteor.users.findOne(user_id);
+        var link = user.services[service_name].link;
+        if (link)
+            Meteor.users.update(user_id, {$set: {"profile.fb_link": link}});
+    }
 };
 
 AccountsMeld.configure({
-	askBeforeMeld: true,
-	meldDBCallback: meldDBCallback
+    askBeforeMeld: true,
+    meldDBCallback: meldDBCallback,
+    serviceAddedCallback: serviceAddedCallback
 });
